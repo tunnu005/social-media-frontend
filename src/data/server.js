f1d244bc652cf3c9e6884c31d624e603898235ef
tunnu005 <tunnuthe005@gmail.com>
2024-11-10 10:58:45 +0530

    serevrapi2

--- src/data/server.js
+++ src/data/server.js
@@ -1,3 +1,2 @@
-
 // export const serverapi = 'http://localhost:8000'
-export const serverapi = 'https://social-media-backend-1w5q.onrender.com'
+export const serverapi = 'https://social-media-backend-1w5q.onrender.com';
