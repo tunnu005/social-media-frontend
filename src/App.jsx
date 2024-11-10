1dec597b644d36f51f73facd2d11481873f5b97a
tunnu005 <tunnuthe005@gmail.com>
2024-11-10 11:58:54 +0530

    socket one

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,9 @@ import AuthenticatedLayout from './utilitis/authlayout';
 import InnerComponent from './component/chatComponent';
 import { io } from 'socket.io-client';
 
-const ChatFull = ({ socket }) => {
-  return (
-    <div className="max-h-screen w-full flex">
-      <InnerComponent socket={socket} />
-    </div>
-  );
-};
-
-function App() {
-  const { isAuthenticated, User } = useContext(AuthContext);
+const ChatFull = () => {
+  const { User } = useContext(AuthContext);
   const [socket, setSocket] = useState(null);
-
   useEffect(() => {
     const newSocket = io('http://localhost:8000');
     setSocket(newSocket);
@@ -40,6 +31,18 @@ function App() {
   }, [User, socket]);
 
   return (
+    <div className="max-h-screen w-full flex">
+      <InnerComponent socket={socket} />
+    </div>
+  );
+};
+
+function App() {
+  const { isAuthenticated } = useContext(AuthContext);
+ 
+
+  
+  return (
     <Router>
       <Routes>
         <Route path="/" element={isAuthenticated ? <Navigate to="/home" /> : <Index />} />
@@ -53,7 +56,7 @@ function App() {
           <Route path="/profile/:userId/:username" element={<Profile />} />
           <Route path="/create" element={<Create />} />
           <Route path="/explore" element={<Explore />} />
-          <Route path="/chat/:userId" element={socket ? <ChatFull socket={socket} /> : null} />
+          <Route path="/chat/:userId" element={socket ? <ChatFull  /> : null} />
         </Route>
       </Routes>
     </Router>
